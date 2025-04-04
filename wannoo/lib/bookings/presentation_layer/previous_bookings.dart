9e8ad0cc82f5ef14a3b4a3d18c0e48ed93fb8109
Mat Gallacher <mat.gallacher@gmail.com>
2025-04-04 20:18:25 +0100

    Add additional categories to empty bookings page

--- wannoo/lib/bookings/presentation_layer/previous_bookings.dart
+++ wannoo/lib/bookings/presentation_layer/previous_bookings.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
@@ -5,22 +7,40 @@ import 'package:wannoo/components/experiences_list_view.dart';
 import 'package:wannoo/components/previous_booking_card.dart';
 import 'package:wannoo/bookings/presentation_layer/previousbookings_controller.dart';
 
-class PreviousBookings extends StatelessWidget {
+class PreviousBookings extends StatefulWidget {
   const PreviousBookings({super.key});
+
+  @override
+  State<PreviousBookings> createState() => _PreviousBookingsState();
+}
+
+class _PreviousBookingsState extends State<PreviousBookings> {
+  final _controller = ScrollController();
+  var _offset = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() => setState(() => _offset = _controller.offset));
+  }
 
   @override
   Widget build(BuildContext context) {
     final PreviousBookingsController previousBookingsController =
         Get.put(PreviousBookingsController());
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Bookings"),
-      ),
       body: Obx(
         () {
-          return previousBookingsController.previousBookings.isNotEmpty
-              ? ListView.builder(
+          return CustomScrollView(
+            controller: _controller,
+            slivers: [
+              const SliverAppBar(
+                title: Text('Bookings'),
+                centerTitle: true,
+                pinned: true,
+              ),
+              if (previousBookingsController.previousBookings.isNotEmpty)
+                SliverList.builder(
                   itemCount: previousBookingsController.previousBookings.length,
                   itemBuilder: (_, int index) {
                     var data =
@@ -38,12 +58,15 @@ class PreviousBookings extends StatelessWidget {
                     );
                   },
                 )
-              : Column(
+              else
+                SliverList.list(
                   children: [
-                    Expanded(
+                    Opacity(
+                      opacity: 1 - min(1, _offset / 180),
                       child: Container(
                         padding: const EdgeInsets.all(32),
                         width: double.infinity,
+                        height: 240,
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           spacing: 16,
@@ -64,25 +87,37 @@ class PreviousBookings extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Expanded(
-                      flex: 2,
-                      child: Column(
-                        children: [
-                          ListTile(
-                            contentPadding: const EdgeInsets.all(16),
-                            title: const Text('Recommended tours'),
-                            titleTextStyle:
-                                Theme.of(context).textTheme.titleLarge,
-                          ),
-                          ExperiencesListview(
-                            filterProperty: 'Trek',
-                            isFilteredList: false,
-                          ),
-                        ],
-                      ),
+                    ListTile(
+                      contentPadding: const EdgeInsets.all(16),
+                      title: const Text('Most Visited'),
+                      titleTextStyle: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    ExperiencesListview(
+                      filterProperty: 'Festivals',
+                      isFilteredList: false,
+                    ),
+                    ListTile(
+                      contentPadding: const EdgeInsets.all(16),
+                      title: const Text('Recommended tours'),
+                      titleTextStyle: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    ExperiencesListview(
+                      filterProperty: 'Trek',
+                      isFilteredList: false,
+                    ),
+                    ListTile(
+                      contentPadding: const EdgeInsets.all(16),
+                      title: const Text('Recommended tours'),
+                      titleTextStyle: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    ExperiencesListview(
+                      filterProperty: 'Trek',
+                      isFilteredList: false,
                     ),
                   ],
-                );
+                ),
+            ],
+          );
         },
       ),
     );
