01e0d33013ec78c4e80bcfe8c613591b7200a733
Mat Gallacher <mat.gallacher@gmail.com>
2025-04-04 11:05:00 +0100

    Add recommended tours to empty bookings page

--- wannoo/lib/bookings/presentation_layer/previous_bookings.dart
+++ wannoo/lib/bookings/presentation_layer/previous_bookings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:wannoo/components/experiences_list_view.dart';
 import 'package:wannoo/components/previous_booking_card.dart';
 import 'package:wannoo/bookings/presentation_layer/previousbookings_controller.dart';
 
@@ -37,25 +38,50 @@ class PreviousBookings extends StatelessWidget {
                     );
                   },
                 )
-              : Container(
-                  padding: const EdgeInsets.all(32),
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 16,
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.ticket,
-                        size: 56,
-                        color: Theme.of(context).colorScheme.onSurfaceVariant,
+              : Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.all(32),
+                        width: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 16,
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.ticket,
+                              size: 56,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSurfaceVariant,
+                            ),
+                            Text(
+                              'No bookings yet\nStart exploring and making memories',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
                       ),
-                      Text(
-                        'No bookings yet\nStart exploring and making memories',
-                        style: Theme.of(context).textTheme.bodyLarge,
-                        textAlign: TextAlign.center,
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            contentPadding: const EdgeInsets.all(16),
+                            title: const Text('Recommended tours'),
+                            titleTextStyle:
+                                Theme.of(context).textTheme.titleLarge,
+                          ),
+                          ExperiencesListview(
+                            filterProperty: 'Trek',
+                            isFilteredList: false,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 );
         },
       ),
