695adb14cd921e86d256a5978d244faa4f8d839d
Aditya Singh Borana <aditya@ogresto.com>
2025-04-06 18:39:42 +0530

    added filters in category places screen, filter Widget and Controller is added

--- wannoo/lib/category_places/presentation_layer/category_places_screen.dart
+++ wannoo/lib/category_places/presentation_layer/category_places_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wannoo/category_places/presentation_layer/category_places_controller.dart';
+import 'package:wannoo/category_places/presentation_layer/filters_widget.dart';
 import 'package:wannoo/components/empty_state.dart';
 import 'package:wannoo/routes.dart';
 import 'package:wannoo/utilities/extension.dart';
@@ -15,39 +16,55 @@ class CategoryPlacesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final categoryPlacesController = Get.put(CategoryPlacesController());
     return Scaffold(
+      endDrawer: Drawer(
+        child: FiltersWidget(),
+      ),
       appBar: AppBar(
         title: Text(category ?? ""),
-      ),
-      body: categoryPlacesController.places.isNotEmpty
-          ? ListView.separated(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              itemCount: categoryPlacesController.places.length,
-              itemBuilder: (context, index) {
-                final item = categoryPlacesController.places[index];
-                return PlacesCard(
-                  id: item.internaTourid,
-                  title: item.title,
-                  image: item.imagepath,
-                  location: item.location,
-                  onTap: () {
-                    Get.toNamed(AppRoutes.placedetails, parameters: {
-                      'amount': categoryPlacesController.places[index].price
-                          .toString(),
-                      'id': "${categoryPlacesController.places[index].id}"
-                    });
-                  },
-                );
+        actions: [
+          Builder(
+            builder: (context) => IconButton(
+              icon: Icon(Icons.tune), // 👈 change this to any icon you want
+              onPressed: () {
+                Scaffold.of(context).openEndDrawer();
               },
-              separatorBuilder: (context, index) => const SizedBox(height: 24),
-            ).fadeIn(
-              duration: const Duration(seconds: 1),
-            )
-          : Center(
-              child: dataNotFound(
-                width: double.infinity,
-                height: 300.00,
-              ),
             ),
+          ),
+        ],
+      ),
+      body: Obx(() {
+        return categoryPlacesController.places.isNotEmpty
+            ? ListView.separated(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                itemCount: categoryPlacesController.places.length,
+                itemBuilder: (context, index) {
+                  final item = categoryPlacesController.places[index];
+                  return PlacesCard(
+                    id: item.internaTourid,
+                    title: item.title,
+                    image: item.imagepath,
+                    location: item.location,
+                    onTap: () {
+                      Get.toNamed(AppRoutes.placedetails, parameters: {
+                        'amount': categoryPlacesController.places[index].price
+                            .toString(),
+                        'id': "${categoryPlacesController.places[index].id}"
+                      });
+                    },
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 24),
+              ).fadeIn(
+                duration: const Duration(seconds: 1),
+              )
+            : Center(
+                child: dataNotFound(
+                  width: double.infinity,
+                  height: 300.00,
+                ),
+              );
+      }),
     );
   }
 }
---
+++ wannoo/lib/category_places/presentation_layer/filters_widget.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wannoo/category_places/presentation_layer/filters_controller.dart';
+import '../../Constants.dart';
+import '../../search/data_layer/model/request/city_request.dart';
+import '../../search/data_layer/model/request/countryrequest.dart';
+import '../../search/presentation_layer/widgets/drop_down.dart';
+import 'category_places_controller.dart';
+
+class FiltersWidget extends StatelessWidget {
+  const FiltersWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final FiltersController filtersController = Get.find();
+    final CategoryPlacesController categoryPlacesController = Get.find();
+    return ListView(
+      children: [
+        ListTile(
+          dense: true,
+          title: const Text("Continent"),
+          titleTextStyle: Theme.of(context).textTheme.titleSmall,
+        ),
+        ListTile(
+          title: CustomDropDownRow(
+            icon: const Icon(Icons.place),
+            dropdownlist: filtersController.continentList,
+            onchanged: (value) {
+              filtersController.selectedContinent.value = value;
+              filtersController.getCountries(CountriesRequest(name: value));
+              // Call the API related to continent selection here
+            },
+          ),
+        ),
+        ListTile(
+          dense: true,
+          title: const Text("Country"),
+          titleTextStyle: Theme.of(context).textTheme.titleSmall,
+        ),
+        ListTile(
+          title: CustomDropDownRow(
+            icon: const Icon(Icons.place),
+            dropdownlist: filtersController.countryList,
+            onchanged: (value) {
+              filtersController.selectedCountry.value = value;
+              filtersController.getCities(CityRequest(countryName: value));
+              // Call the API related to continent selection here
+            },
+          ),
+        ),
+        ListTile(
+          dense: true,
+          title: const Text("City"),
+          titleTextStyle: Theme.of(context).textTheme.titleSmall,
+        ),
+        ListTile(
+          title: CustomDropDownRow(
+            icon: const Icon(Icons.place),
+            dropdownlist: filtersController.cityList,
+            onchanged: (value) {
+              filtersController.selectedCity.value = value;
+            },
+          ),
+        ),
+        ListTile(
+          dense: true,
+          title: const Text("Category"),
+          titleTextStyle: Theme.of(context).textTheme.titleSmall,
+        ),
+        ListTile(
+          title: CustomDropDownRow(
+            icon: const Icon(Icons.category),
+            dropdownlist: filtersController.categoryList,
+            onchanged: (value) {
+              filtersController.selectedCategory.value = value;
+
+              // Call the API related to continent selection here
+            },
+          ),
+        ),
+        const SizedBox(height: 16),
+        ListTile(
+          title: FilledButton(
+            onPressed: () {
+              Navigator.pop(context);
+              categoryPlacesController.getFilteredList();
+            },
+            child: const Text('Search'),
+          ),
+        ),
+        ListTile(
+          title: TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              categoryPlacesController.getlist();
+            },
+            child: const Text('clear'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomRow extends StatelessWidget {
+  final String name;
+
+  const CustomRow({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Initialize with a placeholder value
+
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        // Aligns items to the start of the row
+        children: [
+          Text(
+            name,
+            style: CustomTextStyles.fontMdMedium,
+          ),
+
+          // Add some spacing between the Text and DropdownButton
+        ]);
+  }
+}
+
+class CustomDropDownRow extends StatelessWidget {
+  final RxList<String> dropdownlist;
+  final Icon icon;
+
+  final ValueChanged<String> onchanged;
+
+  const CustomDropDownRow(
+      {Key? key,
+      required this.dropdownlist,
+      required this.onchanged,
+      required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Initialize with a placeholder value
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          boxShadow: [globalShadow],
+          color: ThemeColor.colorWhite,
+          borderRadius: BorderRadius.circular(GlobalRadius.borderRadiusMedium)),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          // Aligns items to the start of the row
+          children: [
+            icon,
+            const SizedBox(
+              width: Height.heightSm,
+            ),
+            Expanded(
+              child: CustomDropDown(
+                list: dropdownlist,
+                onChanged: (String selectedValue) {
+                  onchanged(selectedValue); // Define custom behavior here
+                },
+              ),
+            ),
+            // Add some spacing between the Text and DropdownButton
+          ]),
+    );
+  }
+}
